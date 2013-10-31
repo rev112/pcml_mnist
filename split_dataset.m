f2943c4f4842534c547f9b79839aa04025638077
islijepcevic <islijepcevic@gmail.com>
2013-10-31 11:38:15 +0100

    added a function that splits dataset (matlab)

---
+++ split_dataset.m
@@ -0,0 +1,22 @@
+function split_dataset(filename_in, filename_out)
+
+load(filename_in);
+
+N = size(Xtrain, 1) / 2;
+
+index1 = randperm(N);
+index2 = randperm(N) + N;
+
+i1_train = index1(1 : N*2/3);
+i1_valid = index1(N*2/3 + 1 : end);
+i2_train = index2(1 : N*2/3);
+i2_valid = index2(N*2/3 + 1 : end);
+
+TrainSet = Xtrain([i1_train i2_train], :);
+ValidSet = Xtrain([i1_valid i2_valid], :);
+TrainClass = Ytrain([i1_train i2_train], :);
+ValidClass = Ytrain([i1_valid i2_valid], :);
+TestSet = Xtest;
+TestClass = Ytest;
+
+save(filename_out, 'TrainSet', 'ValidSet', 'TrainClass', 'ValidClass', 'TestSet', 'TestClass');
