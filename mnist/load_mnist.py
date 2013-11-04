49045478f4d62565e8d8aa1c161510b6e8963c8f
Anton Ovchinnikov <revolver112@gmail.com>
2013-11-04 20:00:44 +0400

    Update load_mnist.py script to show the available datasets

--- mnist/load_mnist.py
+++ mnist/load_mnist.py
@@ -4,8 +4,15 @@ from numpy import *
 import scipy.io
 import sys
 
-d = scipy.io.loadmat(sys.argv[1]) # corresponding MAT file
-data = d['Xtrain']    # Xtest for test data
-labels = d['Ytrain']  # Ytest for test labels
+if len(sys.argv) < 2:
+  sys.stderr.write("Usage: ./load_mnist.py MATFILE")
+  sys.exit(1);
 
-print 'Finished loading',data.shape[0],'datapoints'
+d = scipy.io.loadmat(sys.argv[1]) # corresponding MAT file
+keys = filter(lambda x: x[:2] != '__', d.keys())
+keys.sort()
+
+for k in keys:
+  data = d[k]
+
+  print k, 'set: ',data.shape[0],'datapoints'
