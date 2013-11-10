f2e52100a4e1c633fe6237be6933abcc139ffce5
Anton Ovchinnikov <revolver112@gmail.com>
2013-11-10 22:34:14 +0400

    Add derivatives of g

--- mlp.py
+++ mlp.py
@@ -10,6 +10,14 @@ def g(a1, a2):
     """Compute the transfer function (g): a1*sig(a2)"""
     return 1.0 * a1 * sig(a2)
 
+def g_der_1(a1, a2):
+    """Compute the partial derivative of g wrt first argument"""
+    return sig(a2)
+
+def g_der_2(a1, a2):
+    """Compute the partial derivative of g wrt second argument"""
+    return a1 * (1 - sig(a2)) * sig(a2)
+
 class Layer:
     """Class for one hidden layer"""
 
