a5ab9461c5e997ce25410fe3b8ac1ee0cdaddfb7
islijepcevic <islijepcevic@gmail.com>
2013-12-16 21:01:43 +0100

    current script status

--- mlp/multi_run_script.sh
+++ mlp/multi_run_script.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-#ARCHITECTURES='[10] [20] [50] [100] [5,10] [10,5]'
-ARCHITECTURES='[100] [5,10] [10,5]'
+ARCHITECTURES='[10] [20] [50] [100] [5,10] [10,5]'
 
 for arch in $ARCHITECTURES; do
     echo "ARCHITECTURE ${arch}"
     echo ""
-    for k in {1..2}; do
+    for k in {1..1}; do
         python2 train_mlp.py ../mnist/mp_3-5_data_split.mat $arch 
         echo ""
     done
