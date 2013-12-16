2275c5cb756056460f7dfa61d8fceb63b3eab5ff
islijepcevic <islijepcevic@gmail.com>
2013-12-16 15:55:51 +0100

    MLP some additional stuff

---
+++ mlp/README.md
@@ -0,0 +1,18 @@
+Multi Layer Perceptron
+======================
+
+This folder contains the code for MLP.
+
+Most important files:
+
+`mlp.py` - contains **code for MLP**
+
+`functions.py` - some helping functions (mostly math)
+
+`defaults.py` - contains important contsants, like learning rate
+
+`train_mlp.py` - file with **main running code** for training MLP
+
+`evaluate_mlp.py` - file that takes trained MLP and evaluates it on test dataset
+
+`test_gradients.py` - file that runs training with gradient testing flag on
--- mlp/evaluate_mlp.py
+++ mlp/evaluate_mlp.py
@@ -1,24 +1,31 @@
 import pickle
 import sys
+import scipy.io
+import numpy as np
 
+usage = '''Usage:
+python evaluate_mlp.py <dataset_file>
+
+where <dataset_file> should be a .mat file with entries 'TrainSet' and similar
+'''
+ 
 def run_testing(mat_fname):
     matFileContent = scipy.io.loadmat(mat_fname) # corresponding MAT file
-    data = np.array(matFileContent['Xtest'].tolist())
 
     mlp = pickle.load(open('trained_network.dat', 'rb'))
 
-    # TODO: do testing
+    x_test = np.array(matFileContent['TestSet'].tolist())
+    t_test = np.array(matFileContent['TestClass'].tolist())
 
+    print "Test log error and accuracy:"
+    print mlp.get_input_error(x_test, t_test), \
+            mlp.get_accuracy(x_test, t_test), "%"
 
 if __name__ == '__main__':
     lenarg = len(sys.argv)
     if lenarg == 2:
         mat_fname = sys.argv[1]
         run_testing(mat_fname)
-    elif lenarg == 3:
-        mat_fname = sys.argv[1]
-        mlp_fname = sys.argv[2]
-        run_testing(mat_fname, sys.argv[2])
     else:
         print >> sys.stderr, usage
         sys.exit(1)
